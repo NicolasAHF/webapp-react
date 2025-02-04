b8ee4829d8f4d4525624deee5a3887f8190e0a42
Nicolas Hernandez <69226118+NicolasAHF@users.noreply.github.com>
2025-02-04 15:17:22 +0000

    Added components

---
+++ little-lemon/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Products</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
